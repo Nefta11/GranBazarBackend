ab439fcd14da802d0ed0772ef27108340bd0f859
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-22 17:05:52 -0600

    Mejorar la implementación de autenticación JWT: agregar documentación, optimizar la generación y validación de tokens, y eliminar código innecesario.

--- src/Lib/Auth.php
+++ src/Lib/Auth.php
@@ -6,14 +6,24 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 class Auth {
-    private static $secret_key = 'tu_clave_secreta'; // Cambia esto por una clave secreta segura
+    // Clave secreta para firmar los tokens JWT
+    private static $secret_key = '';
+    // Algoritmo de encriptación
     private static $encrypt = ['HS256'];
+    // Identificador único del usuario
     private static $aud = null;
-    private static $minutes = 259200;  // Aumente la expiración del token a 6 meses.
+    // Duración del token en minutos (6 meses)
+    private static $minutes = 259200;
 
+    /**
+     * Genera una identificación única para el usuario.
+     * 
+     * @return string
+     */
     private static function Aud() {
         $aud = '';
 
+        // Obtener la IP del cliente
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             $aud = $_SERVER['HTTP_CLIENT_IP'];
         } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -22,87 +32,122 @@ class Auth {
             $aud = $_SERVER['REMOTE_ADDR'];
         }
 
+        // Añadir información del agente de usuario y el nombre del host
         $aud .= @$_SERVER['HTTP_USER_AGENT'];
         $aud .= gethostname();
 
+        // Crear un hash SHA-1 de la información combinada
         return sha1($aud);
     }
 
+    /**
+     * Crea un token con duración predeterminada y datos suministrados.
+     * 
+     * @param array $data
+     * @return string
+     */
     public static function addToken($data) {
         $time = time();
+        // Información del token
         $token = [
-            'exp' => $time + (60 * self::$minutes),
-            'aud' => self::Aud(),
-            'data' => $data
+            'exp' => $time + (60 * self::$minutes), // Tiempo de expiración
+            'aud' => self::Aud(), // Identificación única del usuario
+            'data' => $data // Datos del usuario
         ];
 
+        // Codificar el token usando la clave secreta y el algoritmo de encriptación
         return JWT::encode($token, self::$secret_key, self::$encrypt[0]);
     }
 
+    /**
+     * Crea un token para recuperación de contraseña con duración de 24 horas.
+     * 
+     * @param array $data
+     * @return string
+     */
     public static function tokRecPass($data) {
         $time = time();
+        // Información del token
         $token = [
-            'exp' => $time + (60 * 60 * 24),
-            'aud' => self::Aud(),
-            'data' => $data
+            'exp' => $time + (60 * 60 * 24), // Tiempo de expiración (24 horas)
+            'aud' => self::Aud(), // Identificación única del usuario
+            'data' => $data // Datos del usuario
         ];
 
+        // Codificar el token usando la clave secreta y el algoritmo de encriptación
         return JWT::encode($token, self::$secret_key, self::$encrypt[0]);
     }
 
+    /**
+     * Crea un token para registro con duración de 1 hora.
+     * 
+     * @param array $data
+     * @return string
+     */
     public static function TokReg($data) {
         $time = time();
+        // Información del token
         $token = [
-            'exp' => $time + (60 * 60),
-            'aud' => self::Aud(),
-            'data' => $data
+            'exp' => $time + (60 * 60), // Tiempo de expiración (1 hora)
+            'aud' => self::Aud(), // Identificación única del usuario
+            'data' => $data // Datos del usuario
         ];
 
+        // Codificar el token usando la clave secreta y el algoritmo de encriptación
         return JWT::encode($token, self::$secret_key, self::$encrypt[0]);
     }
 
+    /**
+     * Decodifica y obtiene los datos del token.
+     * 
+     * @param string $token
+     * @return mixed
+     */
     public static function decData($token) {
+        // Decodificar el token
         $data = self::decodeToken($token);
         if ($data) {
-            return $data->data;
+            return $data->data; // Retornar los datos del usuario
         } else {
             return null;
         }
     }
 
+    /**
+     * Valida si el token es válido.
+     * 
+     * @param string $token
+     * @return bool
+     */
     public static function validateToken($token) {
         if (empty($token) || $token == NULL) {
             return false;
         }
+        // Decodificar el token
         $decode = self::decodeToken($token);
         if ($decode === NULL) {
             return false;
         }
+        // Verificar si el token ha expirado
         if ($decode->exp <= time()) {
             return false;
         }
         return true;
     }
 
+    /**
+     * Decodifica un token JWT.
+     * 
+     * @param string $token
+     * @return mixed
+     */
     private static function decodeToken($token) {
         try {
+            // Decodificar el token usando la clave secreta y el algoritmo de encriptación
             return JWT::decode($token, new Key(self::$secret_key, self::$encrypt[0]));
         } catch (\Exception $e) {
             return null;
         }
     }
 
-    public static function obtenerIP() {
-        $ip = '';
-
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-
-        return $ip;
-    }
 }
--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -9,17 +9,28 @@ use App\Lib\Auth;
 
 class AuthMiddleware
 {
+    /**
+     * Middleware de autenticación para verificar el token JWT.
+     * 
+     * @param Request $request
+     * @param RequestHandler $handler
+     * @return Response
+     */
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
+        // Obtener el encabezado de autorización
         $authorizationHeader = $request->getHeaderLine('Authorization');
         $token = null;
 
+        // Extraer el token del encabezado si está presente
         if (preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)) {
             $token = $matches[1];
         }
 
+        // Validar el token
         $authenticated = Auth::validateToken($token);
 
+        // Si el token no es válido, retornar una respuesta 401 (No autorizado)
         if (!$authenticated) {
             $response = new Response();
             $response->getBody()->write(json_encode([
@@ -30,6 +41,7 @@ class AuthMiddleware
             return $response->withStatus(401);
         }
 
+        // Si el token es válido, continuar con el manejo de la solicitud
         return $handler->handle($request);
     }
 }
