a27f4373fc69d18cee10b255f98a4e2ca650520d
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-26 13:52:45 -0600

    Agregar credenciales de Google en el archivo .env y asegurar el endpoint de autenticación de Google en las rutas de usuario.

--- src/Rutas/UserRoute.php
+++ src/Rutas/UserRoute.php
@@ -9,6 +9,6 @@ return function (App $app) {
         $group->post('/register', UserController::class . ':register');
         $group->post('/auth', UserController::class . ':auth');
         $group->get('/user/{id}', UserController::class . ':getUser');
-        $group->post('/google-auth', UserController::class . ':googleAuth'); // Agregar esta línea
+        $group->post('/google-auth', UserController::class . ':googleAuth'); 
     });
 };
